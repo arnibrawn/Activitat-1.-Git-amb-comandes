003aae3ac00864d745877cd98a875c9f05be10dc
Arnau Payet Coll <arpaco@inspalamos.cat>
2026-09-21 16:26:23 +0200

    Actualització: afegit el cicle formatiu i el fitxer contar.kt

--- ArnauPayet.kt
+++ ArnauPayet.kt
@@ -1,3 +1,3 @@
 fun main() {
-    println("Hola! Benvingut al meu projecte, em dic Arnau Payet.")
+    println("Hola! Benvingut al meu projecte, em dic Arnau Payet i estic cursant el cicle superiror de DAM 2.")
 }
